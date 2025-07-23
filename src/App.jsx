6619629d90ca0c94f39eaa9331bbd8b0700fe505
Sridhar Murthy K Chandra <sridharkc375@gmail.com>
2025-07-23 21:55:41 +0530

    feat : add dynamic header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react'
 import './App.css'
 import Layout from './components/Layout'
+import Header from './components/Header'
+
 
 function App() {
+
+  const date = new Date()
+  let formatedDate = date.toLocaleDateString();
+  formatedDate = formatedDate.replaceAll("/", "·") 
+  console.log(formatedDate) 
+
+  // const [notesList, setNotesList] = useState([])
+  const notesList = [
+    {noteTitle: "Vital Notes", noteCreatedDate: formatedDate},
+    {noteTitle: "Spanish class 101", noteCreatedDate: formatedDate},
+    {noteTitle: "Spanish class 101", noteCreatedDate: formatedDate},
+    {noteTitle: "Spanish class 101", noteCreatedDate: formatedDate},
+  ]
+
+
+  
 
   return (
     <>
       <Layout>
-
+        <Header notesList={notesList} />
       </Layout>
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,27 @@
-const Header = () => {
+const Header = ({ notesList }) => {
+
+  console.log(notesList.length) 
+
   return (
-    <div></div>
+    <div 
+    className="bg-neutral-50/90 w-full h-20 flex justify-between items-center-safe rounded-b-2xl p-2 
+    sm:px-3 sm:pt-2" 
+    >
+      <h1 className="text-sm font-['Press_Start_2P'] text-neutral-50 bg-purple-500 rounded-lg p-1 h-full flex justify-center-safe items-center-safe
+      sm:text-2xl sm:p-2 sm:rounded-xl"
+      >
+        Jottr
+      </h1>
+      <h1 className="text-blue-900 text-2xl font-serif sm:text-4xl">
+      {
+        notesList.length === 0 ? "No notes yet..." : notesList.length === 1 
+        ? 
+          <span>You only have <span>{notesList.length}</span> note</span> 
+          : 
+          <span>You have <span>{notesList.length}</span> notes</span>
+      }
+      </h1>
+    </div>
   )
 }
 
